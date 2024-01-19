89f3b4ce8406b89e3bf76099e0cf28972655c4f8
aryanoffical2706 <aryan.21ug2031@iiitranchi.ac.in>
2024-01-20 00:39:52 +0530

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full Stack Develeper","Content Writer","Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
